161499a1e23dfb9acfd4409b86c3240687cd246c
farandbeyond <connorjh4@gmail.com>
2016-04-26 13:08:30 -0400

    proper battleAction
    odd bug. now fixed

--- src/Background/BattleActions/BattleAction.java
+++ src/Background/BattleActions/BattleAction.java
@@ -13,6 +13,18 @@ import Background.BattleEntity;
  */
 public abstract class BattleAction {
     private BattleEntity caster;
+    private String name,description;
+    public abstract void execute(BattleEntity target);
+    public BattleAction(BattleEntity caster,String name, String description){
+        this.name=name;
+        this.description=description;
+        this.caster=caster;
+    }
+    //gets
+    public BattleEntity getCaster(){return caster;}
+    public int getCasterStat(int statID){return caster.getStat(statID);}
+    public String getName(){return name;}
+    public String getDescription(){return description;}
     public BattleAction(BattleEntity caster){
         this.caster = caster;
     }
