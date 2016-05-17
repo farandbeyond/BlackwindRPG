460525698428f9b919f228345b671b2b5e147d75
farandbeyond <connorjh4@gmail.com>
2016-05-16 22:07:41 -0400

    about to start working on battle. final ajustments prior to

--- src/Background/BattleActions/BattleActionLoader.java
+++ src/Background/BattleActions/BattleActionLoader.java
@@ -18,7 +18,7 @@ public class BattleActionLoader {
     public static final int 
             FIREBALL=0, ICEBLAST=1, QUAKE=2,GUST=3,
             CURE=100,RAISE=101,
-            SLICE=200,
+            ATTACK=200, SLICE=201,
             BRAVERY=300,
             SHELTER=301;
     public static BattleAction loadAction(int actionID){
@@ -29,7 +29,7 @@ public class BattleActionLoader {
             case GUST       :return new DamageSpell("Gust","deals 15-20 Air damage",15,5,ElementHandler.AIR,12);
             case CURE       :return new HealingSpell("Cure","Heals an ally for 10-20hp",10,10,false,5);
             case RAISE      :return new HealingSpell("Raise","Revives an ally with 5hp",5,0,true,20);
-                
+            case ATTACK     :return new PhysicalAction("Attack","The basic attack action everyone has",1,0,StatID.STR,StatID.VIT,ElementHandler.NEUTRAL);    
             case SLICE      :return new PhysicalAction("Slice","A quick slash dealing 10-15dmg",10,5,StatID.STR,StatID.VIT,ElementHandler.NEUTRAL);
             case BRAVERY    :return new EffectSpell("Bravery","25% Str buff",10,Effect.effectLoader(Effect.BUFF, StatID.STR, Buff.TWENTYFIVEP,"Bravery"));
             case SHELTER    :return new EffectSpell("Shelter","25% Vit buff",10,Effect.effectLoader(Effect.BUFF, StatID.VIT, Buff.TWENTYFIVEP, "Shelter"));
